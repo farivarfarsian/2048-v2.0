520e96a08370b386b0b76ab7acb927697f6e8b33
Farivar Farsian <fariwar85@gmail.com>
2025-12-30 21:19:56 +0330

    Setting the basic env for the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 ﻿//Based on https://play2048.co/
 
 #ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN
-    #include <Windows.h>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 #endif
 
 #define SDL_MAIN_USE_CALLBACKS 1
@@ -19,19 +19,22 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
     SDL_SetAppMetadata("2048 v2.0", "1.0", "Game");
 
-    if (!SDL_Init(SDL_INIT_VIDEO)) 
+    if (!SDL_Init(SDL_INIT_VIDEO))
     {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("2048 v2.0", 640, 480, 0, &Window, &Renderer)) 
+    if (!SDL_CreateWindowAndRenderer("2048 v2.0", 640, 480, 0, &Window, &Renderer))
     {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
     SDL_SetRenderLogicalPresentation(Renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    //Background to white
+    SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
 
     return SDL_APP_CONTINUE;
 }
@@ -42,14 +45,25 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
     switch (event->type)
     {
     case SDL_EVENT_QUIT:
-		return SDL_APP_SUCCESS; // End of the program
+        return SDL_APP_SUCCESS; // End of the program
         break;
-    case SDL_EVENT_KEY_DOWN: //Test Input
-        if (event->key.scancode == SDL_SCANCODE_W) 
+    case SDL_EVENT_KEY_DOWN: //Inputs of the Game
+        switch (event->key.scancode)
         {
-			SDL_Log("W Key Pressed");
+        case SDL_SCANCODE_UP:
+            SDL_Log("Up Arrow Key Pressed");
+            break;
+		case SDL_SCANCODE_DOWN:
+            SDL_Log("Down Arrow Key Pressed");
+            break;
+        case SDL_SCANCODE_LEFT:
+            SDL_Log("Left Arrow Key Pressed");
+			break;
+        case SDL_SCANCODE_RIGHT:
+            SDL_Log("Right Arrow Key Pressed");
+            break;
         }
-		break;
+        break;
     }
     return SDL_APP_CONTINUE;
 }
@@ -57,7 +71,11 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 /* Runs each famre (Rendering Stuff Happens) */
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-    return SDL_APP_CONTINUE;  
+	SDL_RenderClear(Renderer);
+
+    SDL_RenderPresent(Renderer);
+
+    return SDL_APP_CONTINUE;
 }
 
 /* Runs on shutdown */
